9d2b2e9e8ec92b291ec3f63718b4266c54e34cc4
Angel Lin <angel.tian.lin@gmail.com>
2013-10-07 22:05:14 -0400

    Expansion of "Click to Move" code of raycasting lab. Goal of game is to keep ball on platform.

---
+++ Assets/Scripts/ExitTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitTrigger : MonoBehaviour {
+	public AudioSource otherObject;
+	public AudioSource beachboys;
+	// Use this for initialization
+	void Start () {
+	beachboys.audio.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	//float timetext = Time.time;
+		//guiText.text = timetext.ToString();
+	}
+	
+	void OnTriggerExit() {
+		guiText.text = "Sorry! You Fell off!";
+		beachboys.audio.Stop();
+		otherObject.audio.Play();
+	}
+		
+}
